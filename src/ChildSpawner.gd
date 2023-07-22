93d9fb96ae7bfa099ddc519bb317c7186707170c
cam-arzo <camoarrow4586@gmail.com>
2023-07-22 21:03:51 +1200

    gameplay easier, door works

--- src/ChildSpawner.gd
+++ src/ChildSpawner.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+export var spawn_offset := Vector2(0, 100)
 
 const ChildResource = preload("res://src/Child.tscn")
 
-func spawn_child():
+# first open door & run timer to spawn child
+# spawn child and run timer to close door
+# close door
+
+func start():
+	$Sprite.play("open")
+	$SpawnChild.start()
+
+func _on_SpawnChild_timeout():
 	# add child
 	var child = ChildResource.instance()
+	
+	child.position = position + spawn_offset
 	
 	var level = get_parent()
 	
@@ -14,3 +25,9 @@ func spawn_child():
 	child.connect("dead", level, "child_death")
 	
 	get_parent().add_child(child)
+	
+	$CloseDoor.start()
+
+
+func _on_CloseDoor_timeout():
+	$Sprite.play("closed")
