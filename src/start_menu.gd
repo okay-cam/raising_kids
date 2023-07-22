e187cb3dcee15ce5a6aeb18adbe787fe25fca992
ToonManink <74220020+ToonManink@users.noreply.github.com>
2023-07-22 16:31:12 +1200

    make change on menu

--- src/start_menu.gd
+++ src/start_menu.gd
@@ -1,8 +1,7 @@
 extends CanvasLayer
 
-
 export var MainGameScene : PackedScene
-
+export var OptionGameScene : PackedScene
 
 func _on_Start_Button_button_up():
 	get_tree().change_scene(MainGameScene.resource_path)
@@ -10,3 +9,7 @@ func _on_Start_Button_button_up():
 
 func _on_Quit_buton_button_up():
 	get_tree().quit()
+
+
+#func _on_Option_button_button_up():
+#	get_tree().change_scene(OptionGameScene.resource_path)
